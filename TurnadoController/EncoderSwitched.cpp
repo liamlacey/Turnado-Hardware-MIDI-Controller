f88a246c68c65997ee090ba67335981bc4b1a4ec
Liam Lacey <liamlacey@hotmail.com>
2018-09-09 11:30:06 +0100

    Added doxygen documentation.

--- TurnadoController/EncoderSwitched.cpp
+++ TurnadoController/EncoderSwitched.cpp
@@ -3,7 +3,7 @@
 EncoderSwitched::EncoderSwitched (uint8_t encPin1, uint8_t encPin2, uint8_t switchPin)
 {
   encoder = new Encoder (encPin1, encPin2);
-  
+
   switchDebouncer = new Bounce (switchPin, DEBOUNCE_TIME);
   pinMode (switchPin, INPUT_PULLUP);
 }
@@ -18,10 +18,10 @@ void EncoderSwitched::update()
 {
   //Check for encoder turn
   //TODO: implement encoder acceleration
-  
+
   int env_val = encoder->read();
-  
-  if (env_val >= 4 || env_val <= - 4) 
+
+  if (env_val >= 4 || env_val <= - 4)
   {
     env_val /= 4;
     this->handle_encoder_change (*this, env_val);
@@ -34,12 +34,12 @@ void EncoderSwitched::update()
   if (switchDebouncer->risingEdge())
   {
     switchState = 0;
-    this->handle_switch_change (*this, switchState); 
+    this->handle_switch_change (*this, switchState);
   }
   else if (switchDebouncer->fallingEdge())
   {
     switchState = 1;
-    this->handle_switch_change (*this, switchState); 
+    this->handle_switch_change (*this, switchState);
   }
 }
 
@@ -56,5 +56,10 @@ void EncoderSwitched::onEncoderChange( void (*function)(EncoderSwitched&, int) )
 void EncoderSwitched::onSwitchChange( void (*function)(EncoderSwitched&, uint8_t) )
 {
   this->handle_switch_change = function;
+}
+
+bool EncoderSwitched::operator==(EncoderSwitched& b)
+{
+  return (this == &b);
 }
 
--- TurnadoController/EncoderSwitched.h
+++ TurnadoController/EncoderSwitched.h
@@ -1,28 +1,63 @@
+/*
+  EncoderSwitched.h - Class for processing switched rotary encoders,
+  built on top of the Teensy Encoder and Bounce classes.
+
+  Created by Liam Lacey, September 2018.
+*/
+
 #ifndef EncoderSwitched_h
 #define EncoderSwitched_h
 
 #include <Encoder.h>
 #include <Bounce.h>
 
+/**
+    A Teensy/Arduino class for processing standard switched rotary encoders.
+
+    To use, simply created instances of the class in your Teensy sketch, assign callback functions
+    to the on...() functions, and call the update() function within your loop() function.
+*/
 class EncoderSwitched
 {
     //=====================================================
   public:
 
+    /** Initialises the object to work with a switched rotary encoder.
+
+        @param encPin1 - Encoder pin 1
+        @param encPin2 - Encoder pin 2
+        @param switchPin - Switch pin
+    */
     EncoderSwitched (uint8_t encPin1, uint8_t encPin2, uint8_t switchPin);
     ~EncoderSwitched();
 
+    /** Reads and update all control values.
+
+        You must call this function from the loop() function in your sketch.
+    */
     void update();
 
+    /** Assigns the function you want to be called when the encoder is turned.
+
+        @param enc - The instance of this class that has detected a change
+        @param enc_value - The encoder change, where 1 = clockwise and -1 = anticlockwise
+    */
     void onEncoderChange( void (*)(EncoderSwitched &enc, int enc_value) );
+
+    /** Assigns the function you want to be called when the switch is pressed/released.
+
+        @param enc - The instance of this class that has detected a change
+        @param enc_value - The new switch value, where 1 = pressed and 0 = released
+    */
     void onSwitchChange( void (*)(EncoderSwitched &enc, uint8_t switch_value) );
 
+    /** Returns the current state of the swith (0-1)
+    */
     uint8_t getSwitchState();
 
-    bool operator==(EncoderSwitched& b)
-    {
-      return (this == &b);
-    }
+    /** Compares the memory address of instances of the class
+    */
+    bool operator==(EncoderSwitched& b);
 
     //=====================================================
   private:
