d98f4c9c99ba1cee0b0a305b38be500ed5ef7546
Liam Lacey <liamlacey@hotmail.com>
2018-09-12 19:34:32 +0100

    commented out all control test code.

--- TurnadoController/TurnadoController.ino
+++ TurnadoController/TurnadoController.ino
@@ -3,61 +3,61 @@
 #include "ThumbJoystick.h"
 #include "PinAllocations.h"
 
-RotaryEncoder encoder (5, 6, 4);
-SwitchControl pushButton (3);
-ThumbJoystick joystick (A0);
+//RotaryEncoder encoder (5, 6, 4);
+//SwitchControl pushButton (3);
+//ThumbJoystick joystick (A0);
 
 void setup() 
 {
   Serial.begin(9600);
   delay(500);
   
-  encoder.onEncoderChange (processEncoderChange);
-  encoder.onSwitchChange (processEncoderSwitchChange);
-  pushButton.onSwitchStateChange (processPushButtonChange);
-  joystick.onJoystickChange (processJoystickChange);
+//  encoder.onEncoderChange (processEncoderChange);
+//  encoder.onSwitchChange (processEncoderSwitchChange);
+//  pushButton.onSwitchStateChange (processPushButtonChange);
+//  joystick.onJoystickChange (processJoystickChange);
 }
 
 void loop() 
 {
-  encoder.update();
-  pushButton.update();
-  joystick.update();
+//  encoder.update();
+//  pushButton.update();
+//  joystick.update();
 
 }
 
 void processEncoderChange (RotaryEncoder &enc, int enc_value)
 {
-  if (enc == encoder)
-  {
-    Serial.print ("Enc1: ");
-    Serial.println (enc_value);
-  }
+//  if (enc == encoder)
+//  {
+//    Serial.print ("Enc1: ");
+//    Serial.println (enc_value);
+//  }
 }
 
-void processEncoderSwitchChange (RotaryEncoder &enc, uint8_t switch_state)
+void processEncoderSwitchChange (RotaryEncoder &enc)
 {
-  if (enc == encoder)
-  {
-    Serial.print ("Enc1 switch: ");
-    Serial.println (switch_state);
-  }
+//  if (enc == encoder)
+//  {
+//    Serial.print ("Enc1 switch: ");
+//    Serial.println (enc.getSwitchState());
+//  }
 }
 
 void processPushButtonChange (SwitchControl &switchControl)
 {
-  if (pushButton == switchControl)
-  {
-    Serial.print ("Push Button: ");
-    Serial.println (switchControl.getSwitchState());
-  }
+//  if (pushButton == switchControl)
+//  {
+//    Serial.print ("Push Button: ");
+//    Serial.println (switchControl.getSwitchState());
+//  }
 }
 
 void processJoystickChange (ThumbJoystick &thumbJoystick, bool isYAxis)
 {
-  if (thumbJoystick == joystick && isYAxis)
-  {
-    Serial.print ("JS: ");
-    Serial.println (thumbJoystick.getYAxisValue());
-  } 
+//  if (thumbJoystick == joystick && isYAxis)
+//  {
+//    Serial.print ("JS: ");
+//    Serial.println (thumbJoystick.getYAxisValue());
+//  } 
 }
