11abea171823b86786187308ec5b2ab9cd6a889e
Liam Lacey <liamlacey@hotmail.com>
2018-09-16 18:11:40 +0100

    Moved DEBUG define to globals.h

--- Code/TurnadoController/Globals.h
+++ Code/TurnadoController/Globals.h
@@ -1,4 +1,8 @@
+//=========================================================================
+//DEV STUFF...
+//#define DEBUG 1
 
+//=========================================================================
 #define NUM_OF_KNOB_CONTROLLERS 9 //Includes dictator mode controller
 //#define DICTATOR_KNOB_CONTROLLER_INDEX 8
 #define NUM_OF_ACTUAL_KNOB_CONTROLLERS 8
