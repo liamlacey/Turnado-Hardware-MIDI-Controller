297e3a8877867b9f4cc6c3ca70e6d66505758997
Liam Lacey <liamlacey@hotmail.com>
2018-10-05 19:49:07 +0100

    Fixed non-centring joystick issue.

--- Code/TurnadoController/ThumbJoystick.cpp
+++ Code/TurnadoController/ThumbJoystick.cpp
@@ -19,21 +19,23 @@ void ThumbJoystick::update()
       (value < 512 + (JS_CENTRE_PLATEAU_VAL / 2)))
   {
     value = 512;
-    yAxisRawValue = value;
   }
 
-  //if we've got a new Y-axis raw value within a range of +/-hysteresis_val
+  //if we've got a new Y-axis raw value within a range of +/-hysteresis_val, or a new centre value
   if ((value - JS_Y_HYSTERESIS_VAL > yAxisRawValue) ||
-      (value + JS_Y_HYSTERESIS_VAL < yAxisRawValue))
+      (value + JS_Y_HYSTERESIS_VAL < yAxisRawValue) ||
+      (value == 512 && yAxisRawValue != 512))
   {
     yAxisRawValue = value;
-    
+
     //map and contrain raw value to user value of +/-127 with plateau values at each end
     if (value > 512)
       value = map (value, 512 + (JS_CENTRE_PLATEAU_VAL / 2), 1023 - JS_EDGE_PLATEAU_VAL, 0, 127);
-     else if (value < 512)
+    else if (value < 512)
       value = map (value, JS_EDGE_PLATEAU_VAL, 512 - (JS_CENTRE_PLATEAU_VAL / 2), -128, 0);
-      
+    else
+      value = 0;
+
     value = constrain (value, -128, 127);
 
     if (value != yAxisUserValue)
@@ -41,7 +43,7 @@ void ThumbJoystick::update()
       yAxisUserValue = value;
       this->handle_joystick_change (*this, true);
     }
-    
+
   } //if (new Y axis value)
 }
 
