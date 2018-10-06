e81fd8e61cca17a3517dfbae839478f820c71a93
Liam Lacey <liamlacey@hotmail.com>
2018-10-06 11:47:20 +0100

    Updated TODO list.

--- Code/TurnadoController/Globals.h
+++ Code/TurnadoController/Globals.h
@@ -27,15 +27,14 @@ enum LcdDisplayModes
    Test all untested code / features
    Comment / document code
    Update settings on top bar of controls display in realtime
-   Processing of MIDI-in CCs from Turnado's knobs.
-   Consider improving LCD display general layouts, colours, etc...
-   Consider drawing menu display from 'update' function rather than on control changes
-
-
+   
    _Future version feature and changes ideas:_
-   Allow internal presets of settings that can be changed with a button combination (LCD ctrl switch + preset buttons?). Display preset number in controls display top bar.
-   Improve knob controller (and dictator) sliders to show the difference between the base value and relative value. E.g Base value shown with a bar, relative value shown as slider value starting at bar position.
-   Implement global setting for auto switching LCD display with control messages
-   Have a global settings option to set control settings to default settings
-   Change LCD drawing code to use relative positions of the LCD size (as opposed to absolute values)
+   - Allow internal presets of settings that can be changed with a button combination (LCD ctrl switch + preset buttons?). Display preset number in controls display top bar.
+   - Improve knob controller (and dictator) sliders on LCS to show the difference between the base value and relative value. E.g Base value shown with a bar, relative value shown as slider value starting at bar position.
+   - Implement global setting for auto switching LCD display with control messages
+   - Have a global settings option to set control settings to default settings
+   - Consider improving LCD display general layouts, colours, etc...
+   - Allow device to be a fully assignable generic MIDI controller, where each control (except for the LCD controls) have completely configurable MIDI messages
+   - Change LCD drawing code to use relative positions of the LCD size (as opposed to absolute values)
+   - Consider drawing menu display from 'update' function rather than on control changes
 */
